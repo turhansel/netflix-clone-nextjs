ad42a2efbe6ee32db4a2eb714e1c8aaac6efffd8
Turhan <turhansel@gmail.com>
2021-04-02 11:38:33 +0300

    google auth added

--- pages/index.js
+++ pages/index.js
@@ -5,26 +5,51 @@ export default function Home() {
   const [session, loading] = useSession(); //nextauth
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-black bg-hero">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-black bg-hero ">
       <Head>
         <title>Netflix Clone</title>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+
         <link rel="icon" href="/favicon.ico" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap"
+          rel="stylesheet"
+        />
       </Head>
+      <header className="flex justify-between w-full px-12 py-8">
+        <div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0.238 0.034 919.406 248.488"
+            className="w-28"
+          >
+            <path
+              fill="#e50914"
+              d="M870.46 118.314l49.184 130.208c-14.495-2.07-28.982-4.663-43.733-6.999l-27.707-71.945-28.468 66.006c-13.973-2.336-27.698-3.114-41.672-4.928l49.955-113.89L782.71.034h41.937l25.362 65.22L877.194.034h42.442zM749.596.034h-38.052v225.71c12.425.779 25.362 1.292 38.052 2.841zm-70.927 223.118c-34.68-2.328-69.37-4.39-104.829-5.177V.035h38.823v181.188c22.264.514 44.52 2.32 66.006 3.355zM532.417 88.305v38.822h-53.06v88.263h-38.3V.034H549.77v38.822h-70.405v49.45h53.06zM375.82 38.856v178.605c-12.946 0-26.14 0-38.83.514V38.856h-40.122V.034H416.19v38.822zM254.94 129.19c-17.08 0-37.274 0-51.769.787v57.715c22.778-1.557 45.556-3.363 68.59-4.141v37.273l-107.412 8.548V.034h107.405v38.822H203.17v52.29c15.017 0 38.052-.778 51.768-.778v38.83zM39.831 107.447V243.08c-13.965 1.557-26.398 3.371-39.593 5.442V.034h37.017L87.724 141.11V.034h38.83V232.47c-13.717 2.336-27.698 3.114-42.45 5.177z"
+            />
+          </svg>
+        </div>
+        <div>
+          {!session ? (
+            <button
+              onClick={() => signIn("google")}
+              className="flex items-center bg-[#e50914] text-white text-sm px-4 py-2 rounded"
+            >
+              Sign In
+            </button>
+          ) : (
+            <button
+              onClick={() => signOut()}
+              className="flex items-center bg-[#e50914] text-white text-sm px-4 py-2 rounded"
+            >
+              Sign Out
+            </button>
+          )}
+        </div>
+      </header>
 
       <main className="flex flex-col items-center justify-center flex-1 px-20 text-center ">
-        {!session && (
-          <>
-            Not signed in <br />
-            <button>Sign in</button>
-          </>
-        )}
-        {session && (
-          <>
-            Signed in as {session.user.email} <br />
-            <button onClick={() => signOut()}>Sign out</button>
-          </>
-        )}
-        <h1 className="text-5xl font-bold text-white max-w-lg">
+        <h1 className="text-5xl font-medium text-white max-w-lg tracking-wide font-sans">
           Unlimited movies, TV shows, and more.
         </h1>
         <h2 className="text-2xl text-white mt-4 mb-8">
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
@@ -7,6 +9,9 @@ module.exports = {
         hero:
           "linear-gradient(rgb(0 0 0 / 60%), rgb(0 0 0 / 60%)), url(/hero-bg.jpeg)",
       }),
+      fontFamily: {
+        sans: ["Roboto"],
+      },
     },
   },
   variants: {
