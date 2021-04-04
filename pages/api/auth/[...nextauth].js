55723aafc8de086a0698627c25a7f33222bd6366
Turhan <turhansel@gmail.com>
2021-04-04 18:28:47 +0300

    update nextauth.js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,12 +3,6 @@ import Providers from "next-auth/providers";
 
 export default NextAuth({
   providers: [
-    Providers.GitHub({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
-  ],
-  providers: [
     Providers.Google({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
